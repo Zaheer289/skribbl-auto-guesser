9be888f9a86a5e13dc1c9e758c5d398a9da2179d
Zack <itszaheer289@gmail.com>
2025-02-26 10:07:14 +0000

    Added hint and description mutation observers and used fetch api to obtain the list of words

--- js/content.js
+++ js/content.js
@@ -1,1 +1,44 @@
-console.log("This is just the beginning!")
+console.log("This is just the beginning!");
+const wordBankURL = "https://wlay.me/Skribblio-Word-Bank/words_en_v1.0.0.json";
+let wordList;
+fetch(wordBankURL).then(data => {
+    return data.json();
+}).then(llist=>{
+    wordList = llist;
+})
+const hintsbox = document.querySelector(".hints .container");
+const descbox = document.querySelector("#game-word");
+const hints = hintsbox.children
+const startObserver = new MutationObserver(entries => {
+    for(const entry of entries){
+        if(entry.target.classList.contains("description") && entry.target.textContent=="GUESS THIS"){
+            console.log(entry);
+            hintObserver.observe(hintsbox,{
+                attributes:true,
+                attributeFilter:["class"],
+                subtree:true
+            })
+        }
+        for(let i=0;i<hints.length-1;i++){
+            hints[i].id = i;
+        }
+    }
+})
+const indexSet = new Set()
+const hintObserver = new MutationObserver(entries => {
+    for(const entry of entries){
+        if(!indexSet.has(entry.target.id)){
+            console.log(entry.target.id);
+            indexSet.add(entry.target.id);
+        }
+        if(indexSet.size == hints.length-1){
+            indexSet.clear();
+            hintObserver.disconnect()
+        }
+        
+    }
+})
+startObserver.observe(descbox,{
+    subtree:true,
+    childList:true
+})
