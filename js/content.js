067f115dcaaa4674fd69259d70fda4849234860c
Zack <itszaheer289@gmail.com>
2025-02-26 10:47:33 +0000

    Added the word filter functionality

--- js/content.js
+++ js/content.js
@@ -1,44 +1,56 @@
 console.log("This is just the beginning!");
 const wordBankURL = "https://wlay.me/Skribblio-Word-Bank/words_en_v1.0.0.json";
-let wordList;
 fetch(wordBankURL).then(data => {
     return data.json();
 }).then(llist=>{
-    wordList = llist;
+    gameLoop(llist);
 })
-const hintsbox = document.querySelector(".hints .container");
-const descbox = document.querySelector("#game-word");
-const hints = hintsbox.children
-const startObserver = new MutationObserver(entries => {
-    for(const entry of entries){
-        if(entry.target.classList.contains("description") && entry.target.textContent=="GUESS THIS"){
-            console.log(entry);
-            hintObserver.observe(hintsbox,{
-                attributes:true,
-                attributeFilter:["class"],
-                subtree:true
-            })
+function gameLoop(llist){
+    let filteredList;
+    const hintsbox = document.querySelector(".hints .container");
+    const descbox = document.querySelector("#game-word");
+    const hints = hintsbox.children
+    const indexSet = new Set()
+    const startObserver = new MutationObserver(entries => {
+        for(const entry of entries){
+            if(entry.target.classList.contains("description") && entry.target.textContent=="GUESS THIS"){
+
+                const wordCount = hints[hints.length-1].textContent.split(" ");
+                const totalLetters = wordCount.reduce((acc,curr) => acc+parseInt(curr),0);
+                filteredList = llist[String(wordCount.length)][String(totalLetters)];
+                console.log(filteredList);
+                hintObserver.observe(hintsbox,{
+                    attributes:true,
+                    attributeFilter:["class"],
+                    subtree:true
+                })
+            }
+            for(let i=0;i<hints.length-1;i++){
+                hints[i].id = i;
+            }
         }
-        for(let i=0;i<hints.length-1;i++){
-            hints[i].id = i;
+    })
+
+    const hintObserver = new MutationObserver(entries => {
+        for(const entry of entries){
+            idIdx = entry.target.id
+            if(!indexSet.has(idIdx)){
+                indexSet.add(idIdx);
+                const letter = entry.target.textContent;
+                filteredList = filteredList.filter(value => {
+                    return value.word[idIdx] === letter;
+                })
+                console.log(filteredList);
+            }
+            if(indexSet.size === hints.length-1){
+                indexSet.clear();
+                hintObserver.disconnect()
+            }
+            
         }
-    }
-})
-const indexSet = new Set()
-const hintObserver = new MutationObserver(entries => {
-    for(const entry of entries){
-        if(!indexSet.has(entry.target.id)){
-            console.log(entry.target.id);
-            indexSet.add(entry.target.id);
-        }
-        if(indexSet.size == hints.length-1){
-            indexSet.clear();
-            hintObserver.disconnect()
-        }
-        
-    }
-})
-startObserver.observe(descbox,{
-    subtree:true,
-    childList:true
-})
+    })
+    startObserver.observe(descbox,{
+        subtree:true,
+        childList:true
+    })
+}
